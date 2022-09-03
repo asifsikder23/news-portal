43998c69f77167bcb7ce8c033e5152550e6aecc4
Md. Asif Ullah Sikder <asifsikder23@gmail.com>
2022-09-03 12:59:25 +0600

    content added

--- js/main.js
+++ js/main.js
@@ -1,18 +1,31 @@
 const loadCategory = async() => {
-    const url = `https://openapi.programming-hero.com/api/news/category/01`
+    const url = `https://openapi.programming-hero.com/api/news/categories`
     const res = await fetch(url);
     const data = await res.json();
-    displayNews(data);
+    console.log(data.data);
+    displayCategory(data.data);
+    
 }
-const displayNews = allNews =>{
-    const newContainer = document.getElementById('news-container');
-    allNews.forEach(news => {
-        newsDiv = document.createElement('div');
-        newsDiv.classList.add ('card');
-        newsDiv.innerHTML =`
-        
-        `
-    });
-}
+const displayCategory=(news =>{
+    const newsContainer = document.getElementById('news-container');
 
-loadCategory('');
+    
+    news.news_category.forEach(news_category => {
+        console.log(news_category);
+        const newsDiv = document.createElement('li');
+        newsDiv.classList.add('li');
+        newsDiv.innerHTML=`
+        <a class=" container text-decoration-none text-black fs-5 fw-bold" href ="">${news_category.category_name}</a>
+        <hr>
+        `;
+        newsContainer.appendChild(newsDiv)
+    });
+})
+        // const displayAllNews = () =>{
+        // const showAll = document.getElementById('show-all-news.length')
+        // const showNews = document.getElementById('show-all-news')
+        // for(const show of showAll.length){
+        
+        // }
+
+loadCategory();
